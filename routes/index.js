9b73a08101aa1746aac7cb9d9d693d85e98e31ad
kevinjang <kevinjang@163.com>
2019-12-18 23:25:45 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
 // var http = express.
 // var io = require('socket.io')
 var Handlebars = require('hbs');
+var io = require('socket.io-client');
 
 Handlebars.registerHelper('kd', function(context, options){
   return context + '_' + new Date();
 });
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-
+  
+  const socket = io('http://localhost:3000/')
   // console.log('handlebars:', Handlebars)
+  console.log('socket:', socket);
 
+  socket.on('message', function(item){
+    console.log('indexjs item:', item);
+  });
+  socket.open();
+  // io.on('connection', function(socket){
+  //   console.log('a user connected in index.js');
+  //   socket.on('message', function(data){
+  //     console.log('www message data:', data);
+  //   });
+  //   socket.on('disconnect', function(){});
+  // })
   res.render('index', { title: 'Express', message: 'hello' });
 });
 
