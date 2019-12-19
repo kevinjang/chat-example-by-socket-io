da603ace0f8900688fb99022504f4494c05ca4c6
kevinjang <kevinjang@163.com>
2019-12-19 17:28:59 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,46 @@
 var express = require('express');
+var app = express();
 var router = express.Router();
-var http = require('http');
+var http = require('http').Server(app);
 // var http = express.
 // var io = require('socket.io')
 var Handlebars = require('hbs');
-var io = require('socket.io-client');
-
+var io = require('socket.io')(http);
+var _socket;
 Handlebars.registerHelper('kd', function(context, options){
   return context + '_' + new Date();
 });
+
+// io.on('connection', function(socket){
+//   console.log('io-on-connection-socket:', socket);
+//   _socket = socket;
+// });
+
+// console.log('io:', io)
+io.on('connection', function(socket){
+  // _socket = socket
+  socket.on('news', function(data){
+    console.log('news data:', data);
+  })
+  // console.log('a user conneced socket:', socket);
+})
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   
-  const socket = io('http://localhost:3000/')
+  // const socket = io('http://localhost:3000/');
+  console.log('io:', io.sockets)
   // console.log('handlebars:', Handlebars)
-  console.log('socket:', socket);
+  console.log('_socket:', _socket);
 
-  socket.on('message', function(item){
-    console.log('indexjs item:', item);
-  });
-  socket.open();
+  // socket.open()
+  // console.log('socket.handshake:', socket.handshake)
+
+  // _socket.on('message', function(item){
+  //   console.log('indexjs item:', item);
+  // });
+  // socket.open();
   // io.on('connection', function(socket){
   //   console.log('a user connected in index.js');
   //   socket.on('message', function(data){
